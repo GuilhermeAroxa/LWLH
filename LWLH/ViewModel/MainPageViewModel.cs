9b93bb13546fd117c396c83fb39fd0d01fff87fa
Guilherme Arôxa <Guilherme.Aroxa@gmail.com>
2019-04-03 23:53:03 -0300

    MainPageReady

---
+++ LWLH/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace LWLH.ViewModel
+{
+    class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            StartCommand = new Command(() =>
+            {
+
+            });
+        }
+        public ICommand StartCommand { get; set; }
+    }
+}
